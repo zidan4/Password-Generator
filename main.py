3fcd685b96fc1bb501fc55b490b3e9f35a9b394f
zidan_1013 <80787878+zidan4@users.noreply.github.com>
2024-10-25 19:08:36 +0300

    Update main.py

--- main.py
+++ main.py
@@ -35,6 +35,6 @@ def generate_password(length=16, nums=1, special_chars=1, uppercase=1, lowercase
     
     return password
     
-if __name == '__main__':
+if __name__ == '__main__':
     new_password = generate_password()
     print('Generated password:', new_password)
